39af8486b2ff021719e26512325c1ac57caba877
R8chel <r8chel@users.noreply.github.com>
2022-05-15 14:55:53 -0600

    Code update

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -1,9 +1,6 @@
 # Cryptocurrency Wallet
 ################################################################################
 
-# This file contains the Ethereum transaction functions that you have created throughout this module’s lessons. By using import statements, you will integrate this `crypto_wallet.py` Python script into the Fintech Finder interface program that is found in the `fintech_finder.py` file.
-
-################################################################################
 # Imports
 import os
 import requests
@@ -20,13 +17,13 @@ from web3.gas_strategies.time_based import medium_gas_price_strategy
 def generate_account():
     """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
     # Fetch mnemonic from environment variable.
-    mnemonic = os.getenv("MNEMONIC")
+    mnemonic = os.getenv('MNEMONIC')
 
     # Create Wallet Object
     wallet = Wallet(mnemonic)
 
     # Derive Ethereum Private Key
-    private, public = wallet.derive_account("eth")
+    private, public = wallet.derive_account('eth')
 
     # Convert private key into an Ethereum account
     account = Account.privateKeyToAccount(private)
@@ -39,7 +36,7 @@ def get_balance(w3, address):
     wei_balance = w3.eth.get_balance(address)
 
     # Convert Wei value to ether
-    ether = w3.fromWei(wei_balance, "ether")
+    ether = w3.fromWei(wei_balance, 'ether')
 
     # Return the value in ether
     return ether
@@ -51,19 +48,19 @@ def send_transaction(w3, account, to, wage):
     w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
 
     # Convert eth amount to Wei
-    value = w3.toWei(wage, "ether")
+    value = w3.toWei(wage, 'ether')
 
     # Calculate gas estimate
-    gasEstimate = w3.eth.estimateGas({"to": to, "from": account.address, "value": value})
+    gasEstimate = w3.eth.estimateGas({'to': to, 'from': account.address, 'value': value})
 
     # Construct a raw transaction
     raw_tx = {
-        "to": to,
-        "from": account.address,
-        "value": value,
-        "gas": gasEstimate,
-        "gasPrice": 0,
-        "nonce": w3.eth.getTransactionCount(account.address)
+        'to': to,
+        'from': account.address,
+        'value': value,
+        'gas': gasEstimate,
+        'gasPrice': 0,
+        'nonce': w3.eth.getTransactionCount(account.address)
     }
 
     # Sign the raw transaction with ethereum account
